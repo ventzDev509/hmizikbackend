8c4f8ec01ea07cae67c0d54152b40d39a664f396
ventzdev <ventzdev@gmail.com>
2026-03-26 23:05:14 -0400

    albuslssaaa

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowController } from './follow.controller';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  controllers: [FollowController],
-  providers: [FollowService, PrismaService],
-  exports: [FollowService], 
+  providers: [FollowService],
+  controllers: [FollowController]
 })
 export class FollowModule {}
