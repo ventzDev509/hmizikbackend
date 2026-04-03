2027fcd7f8c6ebb9ed4a3e221a70af81e382273f
ventzdev <ventzdev@gmail.com>
2026-04-03 14:27:58 -0400

    tesskll

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -69,7 +69,7 @@ export class TracksController {
             req.ip ||
             req.connection?.remoteAddress ||
             req.socket?.remoteAddress;
-            
+            console.log(ip)
 
         // Si w gen AuthGuard, userId ap disponib nan req.user
         const userId = req.user?.id;
