fdf01fa827840edf98e22b9331239238e5151a37
ventzdev <ventzdev@gmail.com>
2026-05-07 11:45:04 -0400

    adxx

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,23 +1,32 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module, Global } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Global() 
+@Global()
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAIL_HOST, 
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule], // Asire w ou gen ConfigModule nan pwojè a
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          port: 587,
+          secure: false, // false paske se pò 587
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASS'),
+          },
+          tls: {
+            // Sa a enpòtan anpil pou evite Timeout sou sèvè cloud yo
+            rejectUnauthorized: false,
+          },
         },
-      },
-      defaults: {
-        from: '"H-Mizik Team" <no-reply@h-mizik.com>',
-      },
+        defaults: {
+          from: '"H-Mizik Team" <no-reply@h-mizik.com>',
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [MailService],
